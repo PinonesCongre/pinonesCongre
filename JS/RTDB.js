d574bb99280546a992cdbb87b58c61314937ae32
PinonesCongre <131477495+PinonesCongre@users.noreply.github.com>
2023-05-27 21:19:12 -0600

    Update RTDB.js

--- JS/RTDB.js
+++ JS/RTDB.js
@@ -12,8 +12,16 @@ infoIcon.addEventListener('click', function() {
 });
 
 //Inicio de Configuracion
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('r 7={8:"9-a-b",c:"0-2.d.3",e:"f://0-2-6-h.i.3/",g:"0-2",k:"0-2.l.3",m:"4",n:"1:4:o:p",q:"j-5"};',28,28,'pinones||congre|com|1031984043314|SR373KE6RX|default|firebaseConfig|apiKey|AIzaSyD9HTmOwCx8y|oJ0VV|zaoYBgQYLAfkmWU|authDomain|firebaseapp|databaseURL|https|projectId|rtdb|firebaseio|G|storageBucket|appspot|messagingSenderId|appId|web|49b38b2c60d69601d3732b|measurementId|const'.split('|'),0,{}))
-
+const firebaseConfig = {
+  apiKey: "AIzaSyD9HTmOwCx8y-oJ0VV-zaoYBgQYLAfkmWU",
+  authDomain: "pinones-congre.firebaseapp.com",
+  databaseURL: "https://pinones-congre-default-rtdb.firebaseio.com/",
+  projectId: "pinones-congre",
+  storageBucket: "pinones-congre.appspot.com",
+  messagingSenderId: "1031984043314",
+  appId: "1:1031984043314:web:49b38b2c60d69601d3732b",
+  measurementId: "G-SR373KE6RX"
+};
 // Inicializa Firebase
 firebase.initializeApp(firebaseConfig);
 
