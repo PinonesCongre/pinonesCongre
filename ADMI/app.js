757d96e7e16927c2641403e21442e498ce35538b
PinonesCongre <131477495+PinonesCongre@users.noreply.github.com>
2024-07-01 22:19:42 -0600

    v11.6
    se corrigieron los errores en el crud , se mejoro tambien el diseño para mobiles, se agrego una guia paso a paso para enviar un informe

--- ADMI/app.js
+++ ADMI/app.js
@@ -9,199 +9,158 @@ $(document).ready(function() {
         appId: "1:1031984043314:web:49b38b2c60d69601d3732b",
         measurementId: "G-SR373KE6RX"
     };    
-    firebase.initializeApp(config); // Inicializamos firebase
+    firebase.initializeApp(firebaseConfig);
 
-    var filaEliminada; // Para capturar la fila eliminada
-    var filaEditada; // Para capturar la fila editada o actualizada
-
-    // Creamos constantes para los iconos editar y borrar    
-    const iconoEditar = '<svg class="bi bi-pencil-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>';
-    const iconoBorrar = '<svg class="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>';
-
+    // Referencia a la base de datos de Firebase
     var db = firebase.database();
     var coleccionInformes = db.ref().child("users");
-         
-    var dataSet = [];// Array para guardar los valores de los campos inputs del form
-    var table = $('#tablaInformes').DataTable({
+    
+    // Iconos para la edición y eliminación
+    const iconoEditar = '<svg class="bi bi-pencil-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>';
+    const iconoBorrar = '<svg class="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>';
+    
+    // Inicialización de DataTable
+    $(document).ready(function() {
+      var table = $('#tablaInformes').DataTable({
+        responsive: true, // Hace que la tabla sea responsive
         pageLength: 6,
         lengthMenu: [[6, 12, 24, -1], [6, 12, 24, 'Todos']],
-        data: dataSet,
-        columnDefs: [
-            {
-                targets: [0], 
-                visible: false, // Ocultamos la columna de ID que es la [0]                        
-            },
-            {
-                targets: 1, // El índice de la columna de nombre, en este caso la segunda columna
-                width: "200px" // Ancho deseado para la columna
-            },
-            {
-                targets: -1,        
-                defaultContent: "<div class='wrapper text-center'><div class='btn-group'><button class='btnEditar btn btn-primary' title='Editar'>"+iconoEditar+"</button><button class='btnBorrar btn btn-danger' title='Borrar'>"+iconoBorrar+"</button></div></div>"
-            }
-        ]	   
-    });
-
-    // Muestra datos de DataTables
-    coleccionInformes.on("child_added", datos => {        
-        dataSet = [datos.key, datos.child("nombre").val(), datos.child("rol").val(), datos.child("participo").val(), datos.child("cursosBiblicos").val(), datos.child("horas").val(), datos.child("superintendente").val(), datos.child("notas").val(), datos.child("fechaEnvio").val()];
-        table.rows.add([dataSet]).draw();
-    });
-
-    // Cambios del hijo 
-    coleccionInformes.on('child_changed', datos => {           
-        dataSet = [datos.key, datos.child("nombre").val(), datos.child("rol").val(), datos.child("participo").val(), datos.child("cursosBiblicos").val(), datos.child("horas").val(), datos.child("superintendente").val(), datos.child("notas").val()];
-        table.row(filaEditada).data(dataSet).draw();
-    });
-
-       // Elimina la fila de datos
-    coleccionInformes.on("child_removed", function() {
-        table.row(filaEliminada.parents('tr')).remove().draw();                     
-    });
-
-    $('form').submit(function(e) {                         
-        e.preventDefault();
-        let id = $.trim($('#id').val());        
-        let nombre = $.trim($('#nombre').val());
-        let rol = $.trim($('#rol').val());
-        let participo = $.trim($('#participo').val());
-        let cursosBiblicos = $.trim($('#cursosBiblicos').val());
-        let horas = $.trim($('#horas').val());
-        let superintendente = $.trim($('#superintendente').val());
-        let notas = $.trim($('#notas').val());                      
-        let idFirebase = id;        
-        if (idFirebase == '') {                      
-            idFirebase = coleccionusers.push().key;
-        };                
-        data = {
-            nombre: nombre,
-            rol: rol,
-            participo: participo,
-            cursosBiblicos: cursosBiblicos,
-            horas: horas,
-            superintendente: superintendente,
-            notas: notas
-        };             
-        actualizacionData = {};
-        actualizacionData[`/${idFirebase}`] = data;
-        coleccionInformes.update(actualizacionData);
-        id = '';        
-        $("form").trigger("reset");
-        $('#modalAltaEdicion').modal('hide');
-    });
-
-    //INFORMES MODAL MODIFICADO
-    $("#tablaInformes").on("click", ".btnEditar", function() {
-        filaEditada = table.row($(this).parents('tr'));
-        let fila = $('#tablaInformes').dataTable().fnGetData($(this).closest('tr'));
-        let id = fila[0];
-        let nombre = $(this).closest('tr').find('td:eq(1)').text();
-        let rol = $(this).closest('tr').find('td:eq(2)').text();
-        let participo = parseInt($(this).closest('tr').find('td:eq(3)').text());
-        let cursosBiblicos = parseInt($(this).closest('tr').find('td:eq(4)').text());
-        let horas = parseInt($(this).closest('tr').find('td:eq(5)').text());
-        let superintendente = $(this).closest('tr').find('td:eq(6)').text();
-        let notas = $(this).closest('tr').find('td:eq(7)').text();
+        columns: [
+          { title: "ID", visible: false },
+          { title: "Nombre" },
+          { title: "Rol" },
+          { title: "Participó" },
+          { title: "Cursos Bíblicos" },
+          { title: "Horas" },
+          { title: "Superintendente" },
+          { title: "Notas" },
+          { title: "Fecha de Envío" },
+          { title: "Acciones", width: "100px" }
+        ]
+      });
     
-        // Opciones para el campo "superintendente"
-        let superintendenteOptions = [
-            'Alfonso.P Grupo 1',
-            'Rafael.G Grupo 2',
-            'Alberto.G Grupo 3',
-            'Josue.T Grupo 4',
-            'Mario.B & Marco.H Grupo 5'
-        ];
+      // Escucha eventos de child_added en Firebase para actualizar la tabla
+      coleccionInformes.on("child_added", function(datos) {
+        var data = datos.val();
+        var key = datos.key;
     
-        // Crear las opciones del campo "superintendente" para el cuadro de diálogo
-        let superintendenteOptionsHTML = superintendenteOptions.map(option => {
-            return '<option value="' + option + '" ' + (superintendente === option ? 'selected' : '') + '>' + option + '</option>';
-        }).join('');
+        table.row.add([
+          key,
+          data.nombre,
+          data.rol,
+          data.participo,
+          data.cursosBiblicos,
+          data.horas,
+          data.superintendente,
+          data.notas,
+          data.fechaEnvio,
+          `<button class='btnEditar btn btn-primary' data-key='${key}' title='Editar'>${iconoEditar}</button>
+           <button class='btnBorrar btn btn-danger' data-key='${key}' title='Borrar'>${iconoBorrar}</button>`
+        ]).draw();
+      });
     
-        // Mostrar cuadro de diálogo de SweetAlert2
+      // Abrir modal de edición al hacer clic en botón de editar
+      $('#tablaInformes').on('click', '.btnEditar', function() {
+        var key = $(this).data('key');
+        cargarDatosEnModal(key);
+      });
+    
+      // Eliminar registro al hacer clic en botón de eliminar
+      $('#tablaInformes').on('click', '.btnBorrar', function() {
+        var key = $(this).data('key');
+        
         Swal.fire({
-            title: 'Editar informe',
-            html:
-                '<div style="text-align: left;">' +
-                '<label for="swal-nombre">Nombre:</label>' +
-                '<input id="swal-nombre" class="swal2-input" value="' + nombre + '">' +
-                '<label for="swal-rol">Rol:</label>' +
-                '<select id="swal-rol" class="swal2-input">' +
-                '<option value="Publicador" ' + (rol === 'Publicador' ? 'selected' : '') + '>Publicador</option>' +
-                '<option value="Precursor Auxiliar" ' + (rol === 'Precursor Auxiliar' ? 'selected' : '') + '>Precursor Auxiliar</option>' +
-                '<option value="Precursor Regular" ' + (rol === 'Precursor Regular' ? 'selected' : '') + '>Precursor Regular</option>' +
-                '</select>' +
-                '<label for="swal-horas">Horas:</label>' +
-                '<input id="swal-horas" class="swal2-input" value="' + horas + '">' +
-                '<label for="swal-cursosBiblicos">Cursos Bíblicos:</label>' +
-                '<input id="swal-cursosBiblicos" class="swal2-input" value="' + cursosBiblicos + '">' +
-                '<label for="swal-participo">Participo:</label>' +
-                '<input id="swal-participo" class="swal2-input" value="' + participo + '">' +
-                '<label for="swal-superintendente">Superintendente:</label>' +
-                '<select id="swal-superintendente" class="swal2-input">' +
-                superintendenteOptionsHTML +
-                '</select>' +
-                '<label for="swal-notas">Notas:</label>' +
-                '<input id="swal-notas" class="swal2-input" value="' + notas + '">' +
-                '</div>',
-            focusConfirm: false,
-            showCancelButton: true,
-            confirmButtonText: 'Guardar',
-            cancelButtonText: 'Cancelar',
-            preConfirm: () => {
-                // Obtener los valores de los campos de entrada del cuadro de diálogo
-                let editedNombre = $('#swal-nombre').val();
-                let editedRol = $('#swal-rol').val();
-                let editedHoras = parseInt($('#swal-horas').val());
-                let editedCursosBiblicos = parseInt($('#swal-cursosBiblicos').val());
-                let editedParticipo = parseInt($('#swal-participo').val());
-                let editedSuperintendente = $('#swal-superintendente').val();
-                let editedNotas = $('#swal-notas').val();
-    
-                // Actualizar los datos en la tabla
-                let newData = [id, editedNombre, editedRol, editedHoras, editedCursosBiblicos, editedParticipo, editedSuperintendente, editedNotas];
-                table.row(filaEditada).data(newData).draw();
-    
-                // Actualizar los datos en la base de datos (Firebase)
-                let informeRef = db.ref('users/' + id);
-                informeRef.update({
-                    nombre: editedNombre,
-                    rol: editedRol,
-                    participo: editedParticipo,
-                    cursosBiblicos: editedCursosBiblicos,
-                    horas: editedHoras,       
-                    superintendente: editedSuperintendente,
-                    notas: editedNotas
-                });
-    
-                // Mostrar alerta de informe actualizado
-                Swal.fire('Informe actualizado', 'Se ha actualizado el informe de ' + editedNombre, 'success');
-            },
-            onOpen: () => {
-                // Establecer estilos personalizados para el cuadro de diálogo
-                Swal.getPopup().style.textAlign = 'left';
-            }
-        });
-    });
-    
-    
-    $("#tablaInformes").on("click", ".btnBorrar", function() {   
-        filaEliminada = $(this);
-        Swal.fire({
-            title: '¿Está seguro de eliminar el informe?',
-            text: "¡Esta operación no se puede revertir!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#d33',
-            cancelButtonColor: '#3085d6',
-            confirmButtonText: 'Borrar'
+          title: '¿Está seguro de que desea eliminar este registro?',
+          text: "¡Esta acción no se puede revertir!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#d33',
+          cancelButtonColor: '#3085d6',
+          confirmButtonText: 'Eliminar',
+          cancelButtonText: 'Cancelar'
         }).then((result) => {
-            if (result.value) {
-                let fila = $('#tablaInformes').dataTable().fnGetData($(this).closest('tr'));            
-                let id = fila[0];            
-                db.ref(`users/${id}`).remove()
-                Swal.fire('¡Eliminado!', 'El informe ha sido eliminado.','success')
-            }
+          if (result.isConfirmed) {
+            db.ref('users/' + key).remove()
+              .then(function() {
+                Swal.fire(
+                  '¡Eliminado!',
+                  'El registro ha sido eliminado correctamente.',
+                  'success'
+                ).then(function() {
+                  location.reload(); // Recargar la página después de eliminar
+                });
+              })
+              .catch(function(error) {
+                console.error("Error al eliminar el registro: ", error);
+                Swal.fire(
+                  '¡Error!',
+                  'Ocurrió un error al intentar eliminar el registro.',
+                  'error'
+                );
+              });
+          }
         });
-    }); 
-
+      });
+    
+      // Función para cargar los datos en el modal de edición
+      function cargarDatosEnModal(key) {
+        var usuarioRef = db.ref('users/' + key);
+    
+        usuarioRef.once('value', function(snapshot) {
+          var usuario = snapshot.val();
+          $('#edit-nombre-completo').val(usuario.nombre);
+          $('#edit-rol').val(usuario.rol);
+          $('#edit-hours').val(usuario.horas);
+          $('#edit-bibleCourse').val(usuario.cursosBiblicos);
+          $('#edit-superintendente').val(usuario.superintendente);
+          
+          // Selecciona la opción correcta en el campo "Participó"
+          $('#edit-participo').val(usuario.participo);
+    
+          $('#edit-key').val(key); // Guarda la clave del registro para identificarlo en la actualización
+        });
+    
+        $('#editModal').modal('show'); // Muestra el modal de edición
+      }
+    
+      // Envío del formulario del modal de edición
+      $('#editForm').submit(function(event) {
+        event.preventDefault();
+    
+        var key = $('#edit-key').val();
+        var updates = {
+          nombre: $('#edit-nombre-completo').val(),
+          rol: $('#edit-rol').val(),
+          horas: parseInt($('#edit-hours').val()),
+          cursosBiblicos: parseInt($('#edit-bibleCourse').val()),
+          superintendente: $('#edit-superintendente').val(),
+          participo: $('#edit-participo').val()
+          // Otros campos según sea necesario
+        };
+    
+        db.ref('users/' + key).update(updates)
+          .then(function() {
+            $('#editModal').modal('hide');
+            Swal.fire({
+              title: '¡Éxito!',
+              text: 'Los cambios han sido guardados correctamente.',
+              icon: 'success'
+            }).then(function() {
+              // Recargar la página después de cerrar el mensaje de éxito
+              location.reload();
+            });
+          })
+          .catch(function(error) {
+            console.error("Error al actualizar los datos: ", error);
+            Swal.fire({
+              title: '¡Oops!',
+              text: 'Ha ocurrido un error al guardar los cambios.',
+              icon: 'error'
+            });
+          });
+      });
+    
+    });
+    
+    
 });
---
+++ JS/systemResponsiveAyuda.js
@@ -0,0 +1,84 @@
+function applyStylesToNavAndFooter(navbar, footer, width, padding) {
+    if (navbar && footer) {
+        navbar.style.width = width;
+        navbar.style.display = 'flex';
+        navbar.style.justifyContent = 'space-between';
+        navbar.style.alignItems = 'center';
+        navbar.style.padding = padding;
+        navbar.style.backgroundColor = '#353a40';
+        navbar.style.color = '#fff';
+
+        footer.style.width = width;
+        footer.style.display = 'flex';
+        footer.style.flexDirection = 'row'; // Mantén la dirección del flex para apilar los elementos
+        footer.style.justifyContent = 'space-between'; // Centra los elementos verticalmente
+        footer.style.backgroundColor = '#353a40';
+        footer.style.color = '#fff';
+        footer.style.fontSize = '14px';
+        footer.style.padding = padding;
+    }
+}
+
+function applyResponsiveStyles() {
+    const width = window.innerWidth;
+
+    const container = document.querySelector('.container');
+    const navbar = document.querySelector('.navbar');
+    const footer = document.querySelector('.footer');
+
+    if (width <= 375) { // iPhone 8 and similar sizes
+        container.style.width = '80%';
+        container.style.marginTop = '45px';
+        applyStylesToNavAndFooter(navbar, footer, '92%', '10px 14px');
+        footer.style.marginTop = '25px';
+        footer.style.flexDirection = 'column';
+        footer.style.alignItems = 'center';
+    } else if (width <= 430) { // iPhone 14 Pro (430px de ancho)
+        container.style.width = '80%';
+        container.style.marginTop = '45px';
+        applyStylesToNavAndFooter(navbar, footer, '93%', '10px 14px');
+        footer.style.marginTop = '25px';
+        footer.style.flexDirection = 'column';
+        footer.style.alignItems = 'center';
+    } else if (width <= 412) { // galaxy s20 ultra (412px de ancho)
+            container.style.width = '45%';
+            container.style.marginTop = '45px';
+            applyStylesToNavAndFooter(navbar, footer, '90%', '10px 14px');
+            footer.style.marginTop = '25px';
+            footer.style.flexDirection = 'column';
+            footer.style.alignItems = 'center';
+    } else if (width <= 768) { // iPad Mini (768px de ancho)
+        container.style.width = '65%';
+        container.style.marginTop = '55px';
+        applyStylesToNavAndFooter(navbar, footer, '95%', '10px 18px');
+        footer.style.marginTop = '35px';
+        footer.style.flexDirection = 'column';
+        footer.style.alignItems = 'center';
+    } else if (width <= 1024) { // iPad Pro (1024px de ancho)
+        container.style.width = '60%';
+        container.style.marginTop = '180px';
+        applyStylesToNavAndFooter(navbar, footer, '96%', '15px 18px');
+        footer.style.marginTop = '260px';
+        footer.style.flexDirection = 'column';
+        footer.style.alignItems = 'center';
+    } else if (width <= 1280) { // Adaptación para resolución de 1280px
+        container.style.width = '45%';
+        container.style.marginTop = '60px';
+        applyStylesToNavAndFooter(navbar, footer, '97%', '10px 20px');
+        footer.style.marginTop = '50px';
+    } else if (width <= 1920) { // Adaptación para Full HD (1920x1080 píxeles)
+        container.style.width = '35%';
+        container.style.marginTop = '70px';
+        applyStylesToNavAndFooter(navbar, footer, '97%', '10px 20px');
+        footer.style.marginTop = '130px';
+    } else {
+        // Ajusta los estilos para pantallas más grandes si es necesario
+        container.style.width = '70%';
+        container.style.marginTop = '60px';
+        applyStylesToNavAndFooter(navbar, footer, '85%', '10px 20px');
+        footer.style.marginTop = '70px';
+    }
+}
+
+window.addEventListener('resize', applyResponsiveStyles);
+document.addEventListener('DOMContentLoaded', applyResponsiveStyles);
